ad172ceaed77c697eeb282d115dae2992d9f0602
Mequyo <mequyo@gmail.com>
2026-05-25 14:43:22 +0200

    refactor: make worker use the world.pack method

--- src/worker.ts
+++ src/worker.ts
@@ -4,36 +4,43 @@ import { TerrainGenerator } from "./terrain-generator";
 import { Sixtuple, WorkerMessageIn, WorkerMessageOut } from "./types";
 import { CHUNK_SIZE } from "./constants";
 import { createMeshes } from "./mesh-utils";
-
-
+import { World } from "./world";
 
 const terraingen = new TerrainGenerator();
 
+const MAX_MESH_SIZE = CHUNK_SIZE ** 3 * 6 * 2; // Currently using 2 ints per vertex, 6 sides per cube
 const MESH_BUFFERS: Sixtuple<Uint32Array> = [
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2), // Currently using 2 ints per vertex
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2),
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2),
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2),
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2),
-  new Uint32Array((CHUNK_SIZE ** 3) * 6 * 2),
+  new Uint32Array(MAX_MESH_SIZE),
+  new Uint32Array(MAX_MESH_SIZE),
+  new Uint32Array(MAX_MESH_SIZE),
+  new Uint32Array(MAX_MESH_SIZE),
+  new Uint32Array(MAX_MESH_SIZE),
+  new Uint32Array(MAX_MESH_SIZE),
 ];
-
 
 self.onmessage = async (e: MessageEvent<WorkerMessageIn>) => {
   const { offset, neighbors } = e.data;
-
   const { blocks, heightmap, amount } = terraingen.generateBlocks(offset);
 
   const meshes = createMeshes(MESH_BUFFERS, blocks, neighbors);
-
   const amountbuffer = new Uint16Array([amount]).buffer;
-
-  const key = ((Math.round(offset[0]) + 512) << 20) | ((Math.round(offset[1]) + 512) << 10) | ((Math.round(offset[2]) + 512) << 0);
+  const key = World.pack(offset[0], offset[1], offset[2]);
 
   self.postMessage(
-    { offset: offset.buffer, key, blocks: blocks.buffer, heightmap: heightmap.buffer, amount: amountbuffer, meshes: meshes.map(mesh => mesh.buffer) } as WorkerMessageOut,
-    [offset.buffer, blocks.buffer, heightmap.buffer, amountbuffer, ...meshes.map(m => m.buffer)],
+    {
+      key,
+      offset: offset.buffer,
+      blocks: blocks.buffer,
+      heightmap: heightmap.buffer,
+      amount: amountbuffer,
+      meshes: meshes.map((mesh) => mesh.buffer),
+    } as WorkerMessageOut,
+    [
+      offset.buffer,
+      blocks.buffer,
+      heightmap.buffer,
+      amountbuffer,
+      ...meshes.map((m) => m.buffer),
+    ],
   );
 };
-
-
